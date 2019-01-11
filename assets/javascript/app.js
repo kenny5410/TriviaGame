32081c28df2432ab13cec915de8e9147b8530316
Kenny <nels5410@gmail.com>
2019-01-10 21:05:38 -0600

    Questions added to the HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//Global Variables
+
+var correctAn;
+var incorrectAn;
+var unanswered;
+var questionOne;
+var questionTwo;
+var questionThree;
+var questionFour;
+
+//Need to write a timer function for 120 seconds
+
+//Need to write the main game function 
