c6886506cd5be7a5a32c5ff45c2f0dd6671393c8
Kenny <nels5410@gmail.com>
2019-01-11 21:43:02 -0600

    timer has been created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    $("#start-btn").on("click", reset);
+  };
+
 //Global Variables
 
 var correctAn;
@@ -8,6 +12,33 @@ var questionTwo;
 var questionThree;
 var questionFour;
 
-//Need to write a timer function for 120 seconds
+var timerNum = 121;
+var intervalID;
+
+//Function for timer counting down from 120 seconds
+
+//$("#start-btn").on("click", reset);
+
+function reset() {
+    timerNum = 120;
+}
+
+function run() {
+    intervalID = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    timerNum--;
+    $("#timer").html(timerNum);
+
+    if (timerNum === 0) {
+        stop();
+    }
+}
+
+function stop() {
+    clearInterval(intervalID);
+}
+run();
 
 //Need to write the main game function 
